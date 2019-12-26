29cc2d2114f1295f681468c8e3d673dc23bb3150
Marc <marceremus@gmail.com>
2019-12-26 18:13:47 +0100

    correction js

--- AFPA-JS/Exemples/_PaginationJQ/js/scripts.js
+++ AFPA-JS/Exemples/_PaginationJQ/js/scripts.js
@@ -5,9 +5,13 @@ $(document).ready(function () {
         projects.on("click", "a.pagination__link", function(e){
             e.preventDefault();
 
+            let titreTop = $("h1");
+
             let page = $(this).attr("href");
             projects.load(page + " .projects", function(){
-
+                $("body, html").animate({
+                    scrollTop: 0
+                }, 300);
             })
         });
 
