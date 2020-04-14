3afe905d6280103702363686f8e043fcb06ae58f
MarcuxLabs <marceremus@gmail.com>
2020-04-14 08:27:35 +0200

    Update agence.php

--- AFPA-PHP/exo/projetImmo/src/views/agence.php
+++ AFPA-PHP/exo/projetImmo/src/views/agence.php
@@ -11,67 +11,68 @@ nav('../views/');
 
     <div class="container mb-5">
 
-        <div class="row mt-5">
-            <div class="col-4">
-                <a href="biens.php">
-                    <button type="button" class="btn btn-outline-dark">
-                        Liste des biens
-                    </button>
-                </a>
+        <div class="text-center">
+            <div class="row mt-5">
+                <div class="col-4">
+                    <a href="biens.php">
+                        <button type="button" class="btn btn-outline-dark">
+                            Liste des biens
+                        </button>
+                    </a>
+                </div>
+
+                <div class="col-4">
+                    <a href="addBien.php">
+                        <button type="button" class="btn btn-outline-dark">
+                            Ajouter un bien
+                        </button>
+                    </a>
+                </div>
+
             </div>
 
-            <div class="col-4">
-                <a href="addBien.php">
-                    <button type="button" class="btn btn-outline-dark">
-                        Ajouter un bien
-                    </button>
-                </a>
+            <div class="row mt-5">
+                <div class="col-4">
+                    <a href="listAgence.php">
+                        <button type="button" class="btn btn-outline-dark">
+                            Liste des agences
+                        </button>
+                    </a>
+                </div>
+
+                <div class="col-4">
+                    <a href="addAgence.php">
+                        <button type="button" class="btn btn-outline-dark">
+                            Ajouter l'agence
+                        </button>
+                    </a>
+                </div>
+
             </div>
 
-        </div>
+            <div class="row mt-5">
 
-        <div class="row mt-5">
-            <div class="col-4">
-                <a href="listAgence.php">
-                    <button type="button" class="btn btn-outline-dark">
-                        Liste des agences
-                    </button>
-                </a>
+                <div class="col-4">
+                    <a href="option.php">
+                        <button type="button" class="btn btn-outline-dark">
+                            Liste des options
+                        </button>
+                    </a>
+                </div>
+
+                <div class="col-4">
+                    <a href="addOption.php">
+                        <button type="button" class="btn btn-outline-dark">
+                            Ajouter l'option
+                        </button>
+                    </a>
+                </div>
+
+
             </div>
-
-            <div class="col-4">
-                <a href="addAgence.php">
-                    <button type="button" class="btn btn-outline-dark">
-                        Ajouter l'agence
-                    </button>
-                </a>
-            </div>
-
-        </div>
-
-        <div class="row mt-5">
-
-            <div class="col-4">
-                <a href="option.php">
-                    <button type="button" class="btn btn-outline-dark">
-                        Liste des options
-                    </button>
-                </a>
-            </div>
-
-            <div class="col-4">
-                <a href="addOption.php">
-                    <button type="button" class="btn btn-outline-dark">
-                        Ajouter l'option
-                    </button>
-                </a>
-            </div>
-
-
         </div>
     </div>
 
-    </div>
 <?php
 
 footer();
